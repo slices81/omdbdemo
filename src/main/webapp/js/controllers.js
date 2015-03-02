f27d76fdb9af1c83a5852ba9e3e99c501b5bf35e
alexdunlop81 <alexdunlop81@yahoo.com>
2015-03-02 11:43:19 +0000

    WS integration

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -4,15 +4,10 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
-  function($scope, $http) {
-	
-    $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data;
-    });
-
-    $scope.orderProp = 'age';
-  }]);
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', function($scope, Phone) {
+	  $scope.phones = Phone.query();
+	  $scope.orderProp = 'age';
+	}]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
---
+++ src/main/webapp/js/services.js
@@ -0,0 +1,8 @@
+var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+
+phonecatServices.factory('Phone', ['$resource',
+  function($resource){
+    return $resource('api/v1/search?Name:=X-Men', {}, {
+      query: {method:'GET', params:{}, isArray:true}
+    });
+  }]);
