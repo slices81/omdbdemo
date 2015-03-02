416a377fa8d6d982602f0939d03adb1a25a92cca
alexdunlop81 <alexdunlop81@yahoo.com>
2015-03-02 14:11:27 +0000

    simplified

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -10,7 +10,5 @@ phonecatServices.factory('Phone', ['$resource',
 
 phonecatServices.factory('Movie', ['$resource',
                                    function($resource){
-                                     return $resource('api/v1/movie/:imdbID', {}, {
-                                       query: {method:'GET', params:{imdbID:'phones'}, isArray:false}
-                                     });
-                                   }]);
+                                     return $resource('api/v1/movie/:imdbID');
+}]);
