e1dfc63af6377b73af3416b5259777d9efe1ecc1
alexdunlop81 <alexdunlop81@yahoo.com>
2015-03-02 15:22:08 +0000

    Use list controller

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -17,7 +17,7 @@ phonecatApp.config(['$routeProvider',
       }).
       when('/phones/:imdbID', {
         templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
+        controller: 'PhoneListCtrl'
       }).
       otherwise({
         redirectTo: '/phones'
