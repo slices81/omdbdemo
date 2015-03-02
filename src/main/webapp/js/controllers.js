c4343974bc5d1eb83936779767af861800e3468c
alexdunlop81 <alexdunlop81@yahoo.com>
2015-03-02 14:06:41 +0000

    ;

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -10,7 +10,7 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', function($sc
 	}]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Movie', function($scope, $routeParams, Movie) {
-	alert($routeParams.imdbID)  
+	alert($routeParams.imdbID); 
 	$scope.phone = Movie.get({imdbID: $routeParams.imdbID}, function(movie) {
 	  });
 
